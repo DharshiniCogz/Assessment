0f2e6513d850675c729742c2a4607d89f5202ffa
DharshiniCogz <Dharshini.T@cognizant.com>
2024-07-17 17:34:22 +0530

    Question 3 modified

--- Question_3/async_callback.js
+++ Question_3/async_callback.js
@@ -1,22 +1,13 @@
 function fetchData(url, callback){
     const error = Math.random() > 0.5;
     console.log(error);
-    setTimeout(() => {
-        if(error){
-            callback(`error`, url);
-        } else {
-            callback(`success`, url);
-        }
-    }, 1000);
+    setTimeout(() => { 
+        error ? callback(`error`, url) : callback(`success`, url);}, 1000);
 }
 
 function handleCallback(message, url){
-    if(message === `error`) {
-        console.log(`Error occured: ${message} ${url}`);
-    } else {
-        console.log(`Response is successful. The URL is ${url} and message is ${message}`);
-    }
+    message === 'error' ? console.log(`Error occured: ${message} ${url}`) : console.log(`Response is successful. The URL is ${url} and message is ${message}`);
 }
 
-const url = 'https://example.com';
+const url = 'https://google.com';
 fetchData(url,handleCallback);
